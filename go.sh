c55502710fc1e6934b80668d3baf5900af5c35a5
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-03-30 13:27:10 -0700

    Build against 1.2.3-R0.1

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-CLASSPATH=../craftbukkit-1.1-R4.jar:../plugins/RadioBeacon.jar javac *.java -Xlint:unchecked -Xlint:deprecation
+CLASSPATH=../craftbukkit-1.2.3-R0.1.jar javac *.java -Xlint:unchecked -Xlint:deprecation
 rm -rf me 
 mkdir -p me/exphc/ExphcTweaks
 mv *.class me/exphc/ExphcTweaks
