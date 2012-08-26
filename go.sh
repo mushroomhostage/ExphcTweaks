7f13c6bf379cc736490450ea667a9e553812293a
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-08-26 11:21:51 -0700

    Update to Forestry r4

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-CLASSPATH=../craftbukkit-1.2.5-R4.1-MCPC-SNAPSHOT-173.jar:../SERVER-alpha-mcpc125/mods/thermalexpansion-1.0.4.3_bc2-mcpc-r1.zip:../SERVER-alpha-mcpc125/mods/industrialcraft2-1.103-mcpc1.2.5-r1.zip:../SERVER-alpha-mcpc125/mods/zforestry-1.4.8.6-mcpc1.2.5.zip javac *.java -Xlint:unchecked -Xlint:deprecation
+CLASSPATH=../craftbukkit-1.2.5-R4.1-MCPC-SNAPSHOT-173.jar:../SERVER-alpha-mcpc125/mods/thermalexpansion-1.0.4.3_bc2-mcpc-r1.zip:../SERVER-alpha-mcpc125/mods/industrialcraft2-1.103-mcpc1.2.5-r1.zip:../SERVER-alpha-mcpc125/mods/zforestry-1.4.8.6-mcpc1.2.5-r4.zip javac *.java -Xlint:unchecked -Xlint:deprecation
 rm -rf me 
 mkdir -p me/exphc/ExphcTweaks
 mv *.class me/exphc/ExphcTweaks
